0fc44ab7dd5d860ab54813c7eaccd0a9314820a8
sulenq <whyimustwriteit@gmail.com>
2024-05-19 12:32:22 +0700

    styling

--- src/components/independent/AdminMiniProfile.tsx
+++ src/components/independent/AdminMiniProfile.tsx
@@ -7,18 +7,20 @@ import {
   Text,
 } from "@chakra-ui/react";
 import { RiArrowDownSLine } from "@remixicon/react";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { getCookie } from "typescript-cookie";
 import { iconSize } from "../../const/sizes";
 
 interface Props extends StackProps {}
 
 export default function AdminMiniProfile({ ...props }: Props) {
-  const dataCookie = getCookie("userData");
   const [data, setData] = useState<any | null>(null);
-  if (dataCookie) {
-    setData(JSON.parse(dataCookie));
-  }
+  useEffect(() => {
+    const dataCookie = getCookie("userData");
+    if (dataCookie) {
+      setData(JSON.parse(dataCookie));
+    }
+  }, []);
 
   return (
     <HStack as={Button} className="btn-solid clicky" pl={3} {...props}>
