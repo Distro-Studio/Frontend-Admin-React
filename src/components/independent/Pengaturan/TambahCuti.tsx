92aa51f0e7a277512dffdc843897498efdc1fb5b
sulenq <whyimustwriteit@gmail.com>
2024-05-23 14:49:30 +0700

    working on pengaturan managemen waktu

---
+++ src/components/independent/Pengaturan/TambahCuti.tsx
@@ -0,0 +1,143 @@
+import {
+  Button,
+  ButtonProps,
+  FormControl,
+  FormErrorMessage,
+  FormLabel,
+  Input,
+  Modal,
+  ModalBody,
+  ModalCloseButton,
+  ModalContent,
+  ModalFooter,
+  ModalHeader,
+  ModalOverlay,
+  useDisclosure,
+} from "@chakra-ui/react";
+import { useFormik } from "formik";
+import { useRef } from "react";
+import * as yup from "yup";
+import backOnClose from "../../../lib/backOnClose";
+import useBackOnClose from "../../../lib/useBackOnClose";
+import FormRequired from "../../form/FormRequired";
+import DatePicker from "../../input/DatePicker";
+
+interface Props extends ButtonProps {}
+
+export default function TambahCuti({ ...props }: Props) {
+  const { isOpen, onOpen, onClose } = useDisclosure();
+  useBackOnClose(isOpen, onClose);
+  const initialRef = useRef(null);
+
+  const formik = useFormik({
+    validateOnChange: false,
+    initialValues: {
+      nama: "",
+      jam_from_hour: "" as any,
+      jam_from_minute: "" as any,
+      jam_to_hour: "" as any,
+      jam_to_minute: "" as any,
+    },
+    validationSchema: yup.object().shape({
+      nama: yup.string().required("Harus diisi"),
+      jam_from_hour: yup.number().required("Harus diisi"),
+      jam_from_minute: yup.number().required("Harus diisi"),
+      jam_to_hour: yup.number().required("Harus diisi"),
+      jam_to_minute: yup.number().required("Harus diisi"),
+    }),
+    onSubmit: (values, { resetForm }) => {
+      console.log(values);
+      const jam_from = new Date(
+        Date.UTC(0, 0, 0, values.jam_from_hour, values.jam_from_minute)
+      );
+      const jam_to = new Date(
+        Date.UTC(0, 0, 0, values.jam_to_hour, values.jam_to_minute)
+      );
+
+      const payload = new FormData();
+      payload.append("nama", values.nama);
+      payload.append("jam_from", jam_from.toString());
+      payload.append("jam_to", jam_to.toString());
+
+      //TODO api tambah kelompok gaji
+    },
+  });
+
+  return (
+    <>
+      <Button
+        className="btn-ap clicky"
+        colorScheme="ap"
+        onClick={onOpen}
+        {...props}
+      >
+        Tambah Cuti
+      </Button>
+
+      <Modal
+        isOpen={isOpen}
+        onClose={() => {
+          backOnClose(onClose);
+        }}
+        initialFocusRef={initialRef}
+        isCentered
+      >
+        <ModalOverlay />
+        <ModalContent>
+          <ModalCloseButton />
+          <ModalHeader ref={initialRef}>Tambah Cuti</ModalHeader>
+          <ModalBody>
+            <form id="tambahUnitKerjaForm" onSubmit={formik.handleSubmit}>
+              <FormControl mb={4} isInvalid={formik.errors.nama ? true : false}>
+                <FormLabel>
+                  Nama Cuti
+                  <FormRequired />
+                </FormLabel>
+                <Input
+                  name="nama"
+                  placeholder="Hari Raya"
+                  onChange={formik.handleChange}
+                  value={formik.values.nama}
+                />
+                <FormErrorMessage>
+                  {formik.errors.nama as string}
+                </FormErrorMessage>
+              </FormControl>
+
+              <FormControl
+                flex={"1 1"}
+                isInvalid={formik.errors.jam_from_hour ? true : false}
+              >
+                <FormLabel>
+                  Maksimal Cuti (Hari)
+                  <FormRequired />
+                </FormLabel>
+                <DatePicker
+                  name="tanggal"
+                  formik={formik}
+                  placeholder="Pilih tanggal hari libur"
+                  noUseBackOnClose
+                />
+
+                <FormErrorMessage>
+                  {formik.errors.jam_from_hour as string}
+                </FormErrorMessage>
+              </FormControl>
+            </form>
+          </ModalBody>
+          <ModalFooter>
+            <Button
+              type="submit"
+              form="tambahUnitKerjaForm"
+              className="btn-ap clicky"
+              colorScheme="ap"
+              w={"100%"}
+            >
+              Simpan
+            </Button>
+          </ModalFooter>
+        </ModalContent>
+      </Modal>
+    </>
+  );
+}
--- src/pages/Pengaturan/PengaturanHariLibur.tsx
+++ src/pages/Pengaturan/PengaturanHariLibur.tsx
@@ -1,12 +1,29 @@
-import { Button, Wrap } from "@chakra-ui/react";
+import {
+  Button,
+  Icon,
+  Input,
+  InputGroup,
+  InputLeftElement,
+  Wrap,
+} from "@chakra-ui/react";
+import { RiDownloadLine, RiSearchLine, RiUploadLine } from "@remixicon/react";
+import { useState } from "react";
 import { Link } from "react-router-dom";
+import TabelHariLibur from "../../components/dependent/Pengaturan/Tabel/TabelHariLibur";
+import TambahHariLibur from "../../components/independent/Pengaturan/TambahHariLibur";
 import CContainer from "../../components/wrapper/CContainer";
 import CWrapper from "../../components/wrapper/CWrapper";
 import { useBodyColor } from "../../const/colors";
 import pengaturanTopNavs from "../../const/pengaturanTopNavs";
-import { responsiveSpacing } from "../../const/sizes";
+import { iconSize, responsiveSpacing } from "../../const/sizes";
 
 export default function PengaturanHariLibur() {
+  // Filter Config
+  const defaultFilterConfig = {
+    search: "",
+  };
+  const [filterConfig, setFilterConfig] = useState<any>(defaultFilterConfig);
+
   return (
     <>
       <CWrapper>
@@ -41,7 +58,48 @@ export default function PengaturanHariLibur() {
             flex={"1 1 600px"}
             overflowX={"auto"}
           >
-            <p>Wakwaw</p>
+            <Wrap w={"100%"} mb={responsiveSpacing} className="tabelConfig">
+              <InputGroup flex={"1 1 165px"}>
+                <InputLeftElement>
+                  <Icon as={RiSearchLine} color={"p.500"} fontSize={iconSize} />
+                </InputLeftElement>
+                <Input
+                  placeholder="Pencarian"
+                  flex={"1 1 0"}
+                  onChange={(e) => {
+                    setFilterConfig((ps: any) => ({
+                      ...ps,
+                      search: e.target.value,
+                    }));
+                  }}
+                  value={filterConfig.search}
+                />
+              </InputGroup>
+
+              <Button
+                flex={"1 1 110px"}
+                variant={"outline"}
+                colorScheme="ap"
+                className="clicky"
+                rightIcon={<Icon as={RiUploadLine} fontSize={iconSize} />}
+              >
+                Export
+              </Button>
+
+              <Button
+                flex={"1 1 110px"}
+                variant={"outline"}
+                colorScheme="ap"
+                className="clicky"
+                rightIcon={<Icon as={RiDownloadLine} fontSize={iconSize} />}
+              >
+                Import
+              </Button>
+
+              <TambahHariLibur flex={"1 1 180px"} />
+            </Wrap>
+
+            <TabelHariLibur filterConfig={filterConfig} />
           </CContainer>
         </Wrap>
       </CWrapper>
