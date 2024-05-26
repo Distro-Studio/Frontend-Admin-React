73f44497053cd15cf8d63aedecd19b43c25136b1
sulenq <whyimustwriteit@gmail.com>
2024-05-26 17:48:14 +0700

    working on riwayat penggajian

--- src/pages/Keuangan/RiwayatPenggajian.tsx
+++ src/pages/Keuangan/RiwayatPenggajian.tsx
@@ -1,5 +1,72 @@
-import React from "react";
+import {
+  Button,
+  Icon,
+  Input,
+  InputGroup,
+  InputLeftElement,
+  Wrap,
+} from "@chakra-ui/react";
+import { RiSearchLine, RiUploadLine } from "@remixicon/react";
+import { useState } from "react";
+import PeriodeTahunRiwayatPenggajian from "../../components/dependent/Keuangan/PeriodeTahunRiwayatPenggajian";
+import TabelPenggajian from "../../components/dependent/Keuangan/Tabel/TabelPenggajian";
+import ImportKaryawanModal from "../../components/independent/Karyawan/ImportKaryawanModal";
+import CContainer from "../../components/wrapper/CContainer";
+import CWrapper from "../../components/wrapper/CWrapper";
+import { useBodyColor } from "../../const/colors";
+import { iconSize, responsiveSpacing } from "../../const/sizes";
 
 export default function RiwayatPenggajian() {
-  return <div>RiwayatPenggajian</div>;
+  // Filter Config
+  const defaultFilterConfig = {
+    search: "",
+    periode_tahun: "",
+  };
+  const [filterConfig, setFilterConfig] = useState<any>(defaultFilterConfig);
+
+  return (
+    <>
+      <CWrapper>
+        <CContainer p={responsiveSpacing} bg={useBodyColor()} borderRadius={12}>
+          <Wrap w={"100%"} mb={responsiveSpacing} className="tabelConfig">
+            <InputGroup flex={"1 1 165px"}>
+              <InputLeftElement>
+                <Icon as={RiSearchLine} color={"p.500"} fontSize={iconSize} />
+              </InputLeftElement>
+              <Input
+                placeholder="Pencarian"
+                flex={"1 1 0"}
+                onChange={(e) => {
+                  setFilterConfig((ps: any) => ({
+                    ...ps,
+                    search: e.target.value,
+                  }));
+                }}
+                value={filterConfig.search}
+              />
+            </InputGroup>
+
+            <PeriodeTahunRiwayatPenggajian
+              filterConfig={filterConfig}
+              setFilterConfig={setFilterConfig}
+            />
+
+            <Button
+              flex={"1 1 110px"}
+              variant={"outline"}
+              colorScheme="ap"
+              className="clicky"
+              rightIcon={<Icon as={RiUploadLine} fontSize={iconSize} />}
+            >
+              Export
+            </Button>
+
+            <ImportKaryawanModal />
+          </Wrap>
+
+          <TabelPenggajian filterConfig={filterConfig} />
+        </CContainer>
+      </CWrapper>
+    </>
+  );
 }
