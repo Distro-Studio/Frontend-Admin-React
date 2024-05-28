7989780a2163463b1091b7ac64fc2ead8e3aac46
sulenq <whyimustwriteit@gmail.com>
2024-05-28 17:22:57 +0700

    styling

--- src/components/independent/Karyawan/AjukanTransferKaryawan.tsx
+++ src/components/independent/Karyawan/AjukanTransferKaryawan.tsx
@@ -1,6 +1,7 @@
 import {
   Button,
   ButtonProps,
+  Checkbox,
   FormControl,
   FormErrorMessage,
   FormLabel,
@@ -12,14 +13,18 @@ import {
   ModalFooter,
   ModalHeader,
   ModalOverlay,
+  SimpleGrid,
   useDisclosure,
 } from "@chakra-ui/react";
+import { useFormik } from "formik";
+import { useRef } from "react";
+import * as yup from "yup";
 import backOnClose from "../../../lib/backOnClose";
 import useBackOnClose from "../../../lib/useBackOnClose";
-import { useRef } from "react";
-import { useFormik } from "formik";
-import * as yup from "yup";
 import FormRequired from "../../form/FormRequired";
+import DatePicker from "../../input/DatePicker";
+import FileInput from "../../input/FileInput";
+import Textarea from "../../input/Textarea";
 
 interface Props extends ButtonProps {}
 
@@ -74,6 +79,8 @@ export default function AjukanTransferKaryawan({ ...props }: Props) {
           backOnClose(onClose);
         }}
         initialFocusRef={initialRef}
+        isCentered
+        size={"lg"}
       >
         <ModalOverlay />
         <ModalContent>
@@ -81,7 +88,7 @@ export default function AjukanTransferKaryawan({ ...props }: Props) {
           <ModalHeader ref={initialRef}>Ajukan Transfer Karyawan</ModalHeader>
           <ModalBody>
             <form id="transferKaryawanForm" onSubmit={formik.handleSubmit}>
-              <FormControl isInvalid={formik.errors.nama ? true : false}>
+              <FormControl mb={4} isInvalid={!!formik.errors.nama}>
                 <FormLabel>
                   Nama Karyawan
                   <FormRequired />
@@ -96,10 +103,97 @@ export default function AjukanTransferKaryawan({ ...props }: Props) {
                   {formik.errors.nama as string}
                 </FormErrorMessage>
               </FormControl>
+
+              <SimpleGrid columns={[1, 2]} gap={4}>
+                <FormControl mb={4} isInvalid={!!formik.errors.tanggal}>
+                  <FormLabel>
+                    Tanggal
+                    <FormRequired />
+                  </FormLabel>
+                  <DatePicker
+                    name="tanggal"
+                    formik={formik}
+                    dateValue={formik.values.tanggal}
+                  />
+                  <FormErrorMessage>
+                    {formik.errors.tanggal as string}
+                  </FormErrorMessage>
+                </FormControl>
+
+                <FormControl mb={4} isInvalid={!!formik.errors.tipe}>
+                  <FormLabel>
+                    Tipe
+                    <FormRequired />
+                  </FormLabel>
+                  <Input />
+                  <FormErrorMessage>
+                    {formik.errors.tipe as string}
+                  </FormErrorMessage>
+                </FormControl>
+              </SimpleGrid>
+
+              <FormControl mb={4} isInvalid={!!formik.errors.dokumen}>
+                <FormLabel>
+                  Dokumen
+                  <FormRequired />
+                </FormLabel>
+                <FileInput name="dokumen" formik={formik} />
+                <FormErrorMessage>
+                  {formik.errors.dokumen as string}
+                </FormErrorMessage>
+              </FormControl>
+
+              <FormControl mb={4} isInvalid={formik.errors.nama ? true : false}>
+                <FormLabel>
+                  Alesan
+                  <FormRequired />
+                </FormLabel>
+                <Textarea
+                  name="alasan"
+                  formik={formik}
+                  placeholder="Jolitos Kurniawan"
+                />
+                <FormErrorMessage>
+                  {formik.errors.nama as string}
+                </FormErrorMessage>
+              </FormControl>
+
+              <FormControl
+                mb={4}
+                isInvalid={!!formik.errors.beri_tahu_manager_direktur}
+              >
+                <Checkbox
+                  colorScheme="ap"
+                  isInvalid={!!formik.errors.beri_tahu_manager_direktur}
+                >
+                  Beritahu Manajer Karyawan dan Direktur Melalui Email
+                </Checkbox>
+                <FormErrorMessage>
+                  {formik.errors.beri_tahu_manager_direktur as string}
+                </FormErrorMessage>
+              </FormControl>
+
+              <FormControl isInvalid={!!formik.errors.beri_tahu_karyawan}>
+                <Checkbox
+                  colorScheme="ap"
+                  isInvalid={!!formik.errors.beri_tahu_karyawan}
+                >
+                  Beritahu Karyawan Melalui Email
+                </Checkbox>
+                <FormErrorMessage>
+                  {formik.errors.beri_tahu_karyawan as string}
+                </FormErrorMessage>
+              </FormControl>
             </form>
           </ModalBody>
           <ModalFooter>
-            <Button w={"100%"} colorScheme="ap" className="btn-ap clicky">
+            <Button
+              type="submit"
+              form="transferKaryawanForm"
+              w={"100%"}
+              colorScheme="ap"
+              className="btn-ap clicky"
+            >
               Simpan
             </Button>
           </ModalFooter>
