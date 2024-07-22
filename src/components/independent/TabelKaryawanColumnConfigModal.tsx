9fe655b5c63ce96808700e1e24038860e5d0990f
sulenq <whyimustwriteit@gmail.com>
2024-07-22 11:01:36 +0700

    styling

--- src/components/independent/TabelKaryawanColumnConfigModal.tsx
+++ src/components/independent/TabelKaryawanColumnConfigModal.tsx
@@ -17,7 +17,7 @@ import {
 } from "@chakra-ui/react";
 import { RiLayoutColumnLine } from "@remixicon/react";
 import { useRef, useState } from "react";
-import { iconSize } from "../../const/sizes";
+import { iconSize, responsiveSpacing } from "../../const/sizes";
 import useTabelKaryawanColumns from "../../global/useTabelKaryawanColumns";
 import useBackOnClose from "../../hooks/useBackOnClose";
 import backOnClose from "../../lib/backOnClose";
@@ -112,116 +112,134 @@ export default function TabelKaryawanColumnConfigModal({
         initialFocusRef={initialRef}
         // scrollBehavior="inside"
         allowPinchZoom
-        size={"lg"}
+        size={"full"}
         blockScrollOnMount={false}
         isCentered
       >
         <ModalOverlay />
-        <ModalContent borderRadius={12}>
+        <ModalContent
+          borderRadius={12}
+          minH={"fit-content"}
+          maxH={"calc(100vh - 32px)"}
+        >
           <ModalHeader ref={initialRef}>
             <DisclosureHeader title={title || "Kolom Tabel Config"} />
           </ModalHeader>
 
           <ModalBody className="scrollY">
-            <Text fontWeight={500} mb={2} opacity={0.6}>
-              Preset Kolom
-            </Text>
-            <SimpleGrid columns={[1, 2, 3]} gap={2}>
-              {presetColumn.map((preset, i) => (
-                <Button
-                  key={i}
-                  // borderRadius={"full"}
-                  className="btn-solid clicky"
-                  onClick={() => {
-                    setSelected(preset.columns);
-                  }}
-                >
-                  {preset.label}
-                </Button>
-              ))}
-            </SimpleGrid>
+            <Wrap spacing={responsiveSpacing}>
+              <CContainer flex={"1 1 300px"}>
+                <Text fontWeight={500} mb={4} opacity={0.6}>
+                  Kolom
+                </Text>
+                <SimpleGrid columns={[3]} gap={2}>
+                  {allColumns.map((option, i) => {
+                    const ok = i !== 0;
+                    return (
+                      ok && (
+                        <Button
+                          key={i}
+                          // flex={"1 1 0"}
+                          // minW={"max-content"}
+                          // borderRadius={"full"}
+                          fontWeight={500}
+                          className={"btn-outline clicky"}
+                          borderColor={
+                            selected && selected.some((item) => item === i)
+                              ? "var(--p500a2)"
+                              : ""
+                          }
+                          bg={
+                            selected && selected.some((item) => item === i)
+                              ? "var(--p500a5) !important"
+                              : ""
+                          }
+                          onClick={() => {
+                            const isSelected =
+                              selected && selected.some((item) => item === i);
+                            let newSelected = selected || [];
 
-            <Text fontWeight={500} mt={4} mb={2} opacity={0.6}>
-              Kolom
-            </Text>
-            <Wrap>
-              {allColumns.map((option, i) => {
-                const ok = i !== 0;
-                return (
-                  ok && (
+                            if (isSelected) {
+                              // Filter out the option if it's already selected
+                              newSelected = newSelected.filter(
+                                (item) => item !== i
+                              );
+                            } else {
+                              // Add the option to the selected array
+                              newSelected = [...newSelected, i];
+                            }
+
+                            setSelected(newSelected);
+                          }}
+                        >
+                          <Text
+                            opacity={
+                              selected && selected.some((item) => item === i)
+                                ? 1
+                                : 0.6
+                            }
+                          >
+                            {option.label}
+                          </Text>
+                        </Button>
+                      )
+                    );
+                  })}
+                </SimpleGrid>
+              </CContainer>
+
+              <CContainer flex={"1 1 300px"}>
+                <Text fontWeight={500} mb={4} opacity={0.6}>
+                  Preset Kolom
+                </Text>
+                <SimpleGrid columns={[1, 2, 3]} gap={2}>
+                  {presetColumn.map((preset, i) => (
                     <Button
                       key={i}
-                      // flex={"1 1 0"}
-                      // minW={"max-content"}
-                      fontWeight={500}
-                      borderRadius={"full"}
-                      className={"btn-outline clicky"}
-                      borderColor={
-                        selected && selected.some((item) => item === i)
-                          ? "var(--p500a2)"
-                          : ""
-                      }
-                      bg={
-                        selected && selected.some((item) => item === i)
-                          ? "var(--p500a5) !important"
-                          : ""
-                      }
+                      // borderRadius={"full"}
+                      className="btn-solid clicky"
                       onClick={() => {
-                        const isSelected =
-                          selected && selected.some((item) => item === i);
-                        let newSelected = selected || [];
-
-                        if (isSelected) {
-                          // Filter out the option if it's already selected
-                          newSelected = newSelected.filter(
-                            (item) => item !== i
-                          );
-                        } else {
-                          // Add the option to the selected array
-                          newSelected = [...newSelected, i];
-                        }
-
-                        setSelected(newSelected);
+                        setSelected(preset.columns);
                       }}
                     >
-                      <Text
-                        opacity={
-                          selected && selected.some((item) => item === i)
-                            ? 1
-                            : 0.6
-                        }
-                      >
-                        {option.label}
-                      </Text>
+                      {preset.label}
                     </Button>
-                  )
-                );
-              })}
+                  ))}
+                </SimpleGrid>
+
+                <Text
+                  fontWeight={500}
+                  mt={responsiveSpacing}
+                  mb={4}
+                  opacity={0.6}
+                >
+                  Urutan Kolom (dari kiri ke kanan)
+                </Text>
+                <Wrap>
+                  {selected.length === 0 && (
+                    <Text opacity={0.4}>Tidak ada kolom yang dipilih</Text>
+                  )}
+                  {selected.map((columnIndex, i) => (
+                    <Badge
+                      textTransform={"none"}
+                      bg={"var(--p500a5)"}
+                      color={"p.500"}
+                      // border={"1px solid var(--p500a2)"}
+                      fontWeight={450}
+                      fontSize={"md"}
+                      key={i}
+                    >
+                      {allColumns[columnIndex].label}
+                    </Badge>
+                  ))}
+                </Wrap>
+              </CContainer>
             </Wrap>
           </ModalBody>
 
           <ModalFooter>
             <CContainer>
-              <Text fontWeight={500} mb={2} opacity={0.6}>
-                Urutan Kolom (dari kiri ke kanan)
-              </Text>
-              <Wrap>
-                {selected.length === 0 && (
-                  <Text opacity={0.4}>Tidak ada kolom yang dipilih</Text>
-                )}
-                {selected.map((columnIndex, i) => (
-                  <Badge
-                    textTransform={"none"}
-                    bg={"var(--divider)"}
-                    fontWeight={400}
-                    key={i}
-                  >
-                    {allColumns[columnIndex].label}
-                  </Badge>
-                ))}
-              </Wrap>
-
-              <ButtonGroup mt={6}>
+              <ButtonGroup>
                 <Button
                   w={"100%"}
                   className="btn-solid clicky"
