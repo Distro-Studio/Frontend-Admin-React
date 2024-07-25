cb5869e1e79e90248c61cc11396d7cee5355b2dc
sulenq <whyimustwriteit@gmail.com>
2024-07-25 16:18:00 +0700

    styling

--- src/pages/Pengaturan/PengaturanKelolaRole.tsx
+++ src/pages/Pengaturan/PengaturanKelolaRole.tsx
@@ -1,14 +1,14 @@
-import { Wrap } from "@chakra-ui/react";
+import { HStack, Wrap } from "@chakra-ui/react";
 import { useState } from "react";
 import ExportModal from "../../components/dependent/ExportModal";
 import ImportModal from "../../components/dependent/ImportModal";
+import SearchComponent from "../../components/dependent/input/SearchComponent";
 import PengaturanNavs from "../../components/dependent/PengaturanNavs";
-import SearchComponent from "../../components/dependent/SearchComponent";
 import TabelKelolaRole from "../../components/dependent/TabelPengaturanKelolaRole";
 import TambahRole from "../../components/independent/TambahRole";
 import CContainer from "../../components/wrapper/CContainer";
 import CWrapper from "../../components/wrapper/CWrapper";
-import { useBodyColor } from "../../const/colors";
+import { useLightDarkColor } from "../../const/colors";
 import { responsiveSpacing } from "../../const/sizes";
 
 export default function PengaturanKelolaRole() {
@@ -19,7 +19,7 @@ export default function PengaturanKelolaRole() {
   const [filterConfig, setFilterConfig] = useState<any>(defaultFilterConfig);
 
   // SX
-  // const lightDarkColor = useLightDarkColor();
+  const lightDarkColor = useLightDarkColor();
 
   return (
     <>
@@ -35,20 +35,31 @@ export default function PengaturanKelolaRole() {
           <PengaturanNavs activeTopNavs={0} active={0} />
 
           <CContainer
-            p={responsiveSpacing}
+            px={responsiveSpacing}
+            pb={responsiveSpacing}
+            pt={0}
             h={"100%"}
             overflowY={"auto"}
             className="scrollY"
-            bg={useBodyColor()}
+            bg={lightDarkColor}
             borderRadius={12}
             flex={"1 1 600px"}
           >
-            <Wrap w={"100%"} mb={responsiveSpacing} className="tabelConfig">
+            <HStack
+              py={responsiveSpacing}
+              justify={"space-between"}
+              w={"100%"}
+              className="tabelConfig scrollX"
+              overflowX={"auto"}
+              flexShrink={0}
+            >
               <SearchComponent
-                search={filterConfig.search}
-                setSearch={(newSearch) => {
-                  setFilterConfig((ps: any) => ({ ...ps, search: newSearch }));
+                minW={"165px"}
+                name="search"
+                onChangeSetter={(input) => {
+                  setFilterConfig(input);
                 }}
+                inputValue={filterConfig.search}
               />
 
               <ExportModal url="" title="Export Kelola Role" />
@@ -56,7 +67,7 @@ export default function PengaturanKelolaRole() {
               <ImportModal url="" title="Import Kelola Role" />
 
               <TambahRole minW={"fit-content"} />
-            </Wrap>
+            </HStack>
 
             <TabelKelolaRole filterConfig={filterConfig} />
           </CContainer>
