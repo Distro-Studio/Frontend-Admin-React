f7d82cf34c40562820551a7c5e4de04899d62b9f
sulenq <whyimustwriteit@gmail.com>
2024-07-27 12:14:03 +0700

    working on pengaturan ter pph

--- src/components/dependent/TabelPengaturanTerPph21.tsx
+++ src/components/dependent/TabelPengaturanTerPph21.tsx
@@ -1,280 +1,240 @@
 import {
+  Box,
+  Center,
   HStack,
   Icon,
-  IconButton,
-  Table,
-  Tbody,
-  Td,
+  MenuItem,
   Text,
-  Th,
-  Thead,
-  Tr,
-  VStack,
+  Tooltip,
 } from "@chakra-ui/react";
-import { RiArrowDownLine, RiArrowUpLine, RiMore2Fill } from "@remixicon/react";
-import { useState } from "react";
-import { useBodyColor, useContentBgColor } from "../../const/colors";
+import { RiDeleteBinLine, RiEditLine, RiHistoryLine } from "@remixicon/react";
 import { dummyTerPph21 } from "../../const/dummy";
-import { Tabel__Column__Interface } from "../../const/interfaces";
-import { iconSize } from "../../const/sizes";
+import { iconSize, responsiveSpacing } from "../../const/sizes";
+import { Interface__SelectOption } from "../../constant/interfaces";
+import useDataState from "../../hooks/useDataState";
+import formatDate from "../../lib/formatDate";
 import formatNumber from "../../lib/formatNumber";
 import NoData from "../independent/NoData";
-import ComponentSpinner from "../independent/ComponentSpinner";
-import TabelContainer from "../wrapper/CustomTableContainer";
+import NotFound from "../independent/NotFound";
+import Skeleton from "../independent/Skeleton";
+import CustomTableContainer from "../wrapper/CustomTableContainer";
+import BooleanBadge from "./BooleanBadge";
+import CustomTable from "./CustomTable";
+import Retry from "./Retry";
 
 interface Props {
   filterConfig?: any;
 }
 
 export default function TabelPengaturanTerPph21({ filterConfig }: Props) {
-  const columns: Tabel__Column__Interface[] = [
-    {
-      key: "kategori_ter",
-      label: "Kategori TER",
-      dataType: "string",
+  // SX
+
+  // Row Options Config
+  const rowOptions = [
+    (rowData: any) => {
+      return (
+        <MenuItem>
+          <Text>Edit</Text>
+          <Icon as={RiEditLine} fontSize={iconSize} opacity={0.4} />
+        </MenuItem>
+      );
     },
-    {
-      key: "kode_ptkp",
-      label: "PTKP",
-      dataType: "string",
+    (rowData: any) => {
+      return (
+        <MenuItem isDisabled={!rowData.columnsFormat[1].value}>
+          <Text>Restore</Text>
+          <Icon as={RiHistoryLine} fontSize={iconSize} opacity={0.4} />
+        </MenuItem>
+      );
     },
-    {
-      key: "from_ter",
-      label: "Penghasilan Bruto Bulanan",
-      dataType: "number",
-      preferredTextAlign: "center",
-    },
-    {
-      key: "percentage_ter",
-      label: "Rate TER",
-      dataType: "numeric",
+    "divider",
+    (rowData: any) => {
+      return (
+        <MenuItem fontWeight={500}>
+          <Text color={"red.400"}>Delete</Text>
+          <Icon color={"red.400"} as={RiDeleteBinLine} fontSize={iconSize} />
+        </MenuItem>
+      );
     },
   ];
 
-  //! DEBUG
-  // console.log(filterConfig);
-
-  //! DEBUG
-
-  //TODO get data ter pph21
-
-  const [data] = useState<any[] | null>(dummyTerPph21);
-  const [loading] = useState<boolean>(false);
-
-  // Filter Config
-  const fd = data?.filter((d) => {
-    const searchTerm = filterConfig.search.toLowerCase();
-    const ok =
-      d.id.toString().toLowerCase().includes(searchTerm) ||
-      d.kategori_ters?.nama_kategori_ter.toLowerCase().includes(searchTerm);
-
-    return ok;
+  const { error, loading, data, retry } = useDataState<any[]>({
+    initialData: dummyTerPph21,
+    url: "",
+    dependencies: [],
   });
 
-  // Sort Config
-  const [sortConfig, setSortConfig] = useState<{
-    key: string;
-    direction: "asc" | "desc";
-  } | null>({ key: columns[0].key, direction: "asc" });
-  const sortedData = fd && [...fd];
-  if (sortConfig !== null && sortedData) {
-    sortedData.sort((a, b) => {
-      let aValue = a[sortConfig.key];
-      let bValue = b[sortConfig.key];
+  const fd = data?.filter((item: any) => {
+    const searchTerm = filterConfig?.search.toLowerCase();
+    const isDeletedTerm = filterConfig?.is_deleted?.map(
+      (term: Interface__SelectOption) => term.value
+    );
 
-      // Handle nested properties
-      if (sortConfig.key === "kode_ptkp") {
-        aValue = a.ptkps?.kode_ptkp;
-        bValue = b.ptkps?.kode_ptkp;
-      } else if (sortConfig.key === "kategori_ter") {
-        aValue = a.kategori_ters?.nama_kategori_ter;
-        bValue = b.kategori_ters?.nama_kategori_ter;
-      }
+    const matchesSearchTerm = item.ptkp.kode_ptkp
+      .toLowerCase()
+      .includes(searchTerm);
+    const matchesIsDeletedTerm =
+      isDeletedTerm?.includes(1) && isDeletedTerm?.includes(0)
+        ? true
+        : isDeletedTerm?.includes(1)
+        ? !!item.deleted_at
+        : isDeletedTerm?.includes(0)
+        ? !item.deleted_at
+        : true;
 
-      if (aValue < bValue) {
-        return sortConfig.direction === "asc" ? -1 : 1;
-      }
-      if (aValue > bValue) {
-        return sortConfig.direction === "asc" ? 1 : -1;
-      }
-      return 0;
-    });
-  }
-  const sort = (key: string) => {
-    let direction: "asc" | "desc" = "asc";
-    if (
-      sortConfig &&
-      sortConfig.key === key &&
-      sortConfig.direction === "asc"
-    ) {
-      direction = "desc";
-    }
-    setSortConfig({ key, direction });
-  };
+    return matchesSearchTerm && matchesIsDeletedTerm;
+  });
 
-  // SX
-  const contentBgColor = useContentBgColor();
-  const bodyColor = useBodyColor();
+  const formattedHeader = [
+    {
+      th: "Kode PTKP",
+      isSortable: true,
+      props: {
+        position: "sticky",
+        left: 0,
+        zIndex: 2,
+      },
+      cProps: {
+        borderRight: "1px solid var(--divider3)",
+      },
+    },
+    {
+      th: "Status Dihapus",
+      isSortable: true,
+      cProps: {
+        justify: "center",
+      },
+    },
+    // {
+    //   th: "Tanggal Dihapus",
+    //   isSortable: true,
+    // },
+    {
+      th: "Penghasilan Bruto Bulanan",
+      isSortable: true,
+      cProps: {
+        justify: "center",
+      },
+    },
+    {
+      th: "Persentase TER",
+      isSortable: true,
+      cProps: {
+        justify: "center",
+      },
+    },
+  ];
+  const formattedData = fd?.map((item: any) => ({
+    id: item.id,
+    columnsFormat: [
+      {
+        value: item.ptkp.kode_ptkp,
+        td: item.ptkp.kode_ptkp,
+        isSortable: true,
+        props: {
+          position: "sticky",
+          left: 0,
+          zIndex: 2,
+        },
+        cProps: {
+          borderRight: "1px solid var(--divider3)",
+        },
+      },
+      {
+        value: item.deleted_at,
+        td: item.deleted_at ? (
+          <Tooltip
+            openDelay={500}
+            label={
+              <>
+                <Text>Dihapus {formatDate(item.deleted_at)}</Text>
+
+                <Text opacity={0.4} mt={2}>
+                  Data yang dihapus tidak akan muncul di dafta opsi, namun akan
+                  tetap menjadi legacy data
+                </Text>
+              </>
+            }
+            placement="right"
+          >
+            <Box>
+              <BooleanBadge
+                w={"120px"}
+                data={item.deleted_at}
+                trueValue="Dihapus"
+                falseValue=""
+                colorScheme={item.deleted_at ? "red" : ""}
+              />
+            </Box>
+          </Tooltip>
+        ) : (
+          ""
+        ),
+        isDate: true,
+        cProps: {
+          justify: "center",
+        },
+      },
+      {
+        value: item.from_ter,
+        td: `Rp ${formatNumber(item.from_ter)} - Rp ${formatNumber(
+          item.to_ter
+        )}`,
+        isNumeric: true,
+      },
+      {
+        value: item.percetage_ter,
+        td: `${item.percentage_ter}%`,
+        isNumeric: true,
+        cProps: {
+          justify: "center",
+        },
+      },
+    ],
+  }));
 
   return (
     <>
-      {loading && <ComponentSpinner mt={4} />}
-
-      {!loading && sortedData && (
+      {error && (
+        <Center my={"auto"} minH={"400px"}>
+          <Retry loading={loading} retry={retry} />
+        </Center>
+      )}
+      {!error && (
         <>
-          <TabelContainer noFooterConfig>
-            {fd && fd.length === 0 && <NoData />}
+          {loading && (
+            <>
+              <Skeleton minH={"300px"} flex={1} mx={"auto"} />
+              <HStack justify={"space-between"} mt={responsiveSpacing}>
+                <Skeleton maxW={"120px"} />
+                <Skeleton maxW={"300px"} h={"20px"} />
+                <Skeleton maxW={"112px"} />
+              </HStack>
+            </>
+          )}
+          {!loading && (
+            <>
+              {!formattedData && <NoData minH={"400px"} />}
 
-            {fd && fd.length > 0 && (
-              <Table minW={"100%"}>
-                <Thead>
-                  <Tr position={"sticky"} top={0} zIndex={3}>
-                    {columns.map((column, i) => (
-                      <Th
-                        key={i}
-                        whiteSpace={"nowrap"}
-                        onClick={() => {
-                          if (column.dataType !== "action") {
-                            sort(column.key);
-                          }
-                        }}
-                        cursor={"pointer"}
-                        borderBottom={"none !important"}
-                        bg={bodyColor}
-                        zIndex={2}
-                        p={0}
-                        {...column.thProps}
-                      >
-                        {column.dataType === "action" ||
-                        column.dataType === "link" ? (
-                          <HStack
-                            justify={"center"}
-                            borderBottom={"1px solid var(--divider3)"}
-                            px={4}
-                            py={3}
-                            h={"52px"}
-                            pl={i === 0 ? 4 : ""}
-                            pr={i === columns.length - 1 ? 4 : ""}
-                            {...column.thContentProps}
-                          >
-                            <Text>{column.label}</Text>
-                          </HStack>
-                        ) : (
-                          <HStack
-                            justify={
-                              column.preferredTextAlign === "center"
-                                ? "center"
-                                : column.dataType === "numeric"
-                                ? "flex-end"
-                                : "space-between"
-                            }
-                            borderBottom={"1px solid var(--divider3)"}
-                            px={4}
-                            py={3}
-                            h={"52px"}
-                            pl={i === 0 ? 4 : ""}
-                            pr={i === columns.length - 1 ? 4 : ""}
-                            {...column.thContentProps}
-                          >
-                            <Text
-                              fontWeight={600}
-                              flexShrink={0}
-                              lineHeight={1.2}
-                            >
-                              {column.label}
-                            </Text>
+              {formattedData && (
+                <>
+                  {fd && fd?.length === 0 && <NotFound minH={"400px"} />}
 
-                            {sortConfig && sortConfig.key === column.key && (
-                              <>
-                                {sortConfig.direction === "asc" ? (
-                                  <Icon
-                                    as={RiArrowUpLine}
-                                    color={"p.500"}
-                                    fontSize={16}
-                                  />
-                                ) : (
-                                  <Icon
-                                    as={RiArrowDownLine}
-                                    color={"p.500"}
-                                    fontSize={16}
-                                  />
-                                )}
-                              </>
-                            )}
-                          </HStack>
-                        )}
-                      </Th>
-                    ))}
-
-                    {/* Kolom tetap di sebelah kanan */}
-                    <Th
-                      position={"sticky"}
-                      top={0}
-                      right={0}
-                      borderBottom={"none !important"}
-                      p={0}
-                      bg={bodyColor}
-                      zIndex={2}
-                    >
-                      <VStack
-                        px={4}
-                        py={3}
-                        zIndex={99}
-                        borderLeft={"1px solid var(--divider3)"}
-                        borderBottom={"1px solid var(--divider3)"}
-                        h={"52px"}
-                      ></VStack>
-                    </Th>
-                  </Tr>
-                </Thead>
-
-                <Tbody>
-                  {sortedData.map((row, i) => (
-                    <Tr key={i} bg={i % 2 === 0 ? contentBgColor : bodyColor}>
-                      <Td whiteSpace={"nowrap"}>
-                        {row.kategori_ters.nama_kategori_ter}
-                      </Td>
-                      <Td whiteSpace={"nowrap"}>{row.ptkps.kode_ptkp}</Td>
-                      <Td whiteSpace={"nowrap"} textAlign={"center"}>
-                        {`Rp.${formatNumber(row.from_ter)} - Rp.${formatNumber(
-                          row.to_ter
-                        )}`}
-                      </Td>
-                      <Td whiteSpace={"nowrap"} textAlign={"right"}>
-                        {row.percentage_ter}%
-                      </Td>
-
-                      {/* Kolom tetap di sebelah kanan */}
-                      <Td
-                        position={"sticky"}
-                        top={0}
-                        right={0}
-                        borderBottom={"none !important"}
-                        p={0}
-                        bg={i % 2 === 0 ? contentBgColor : bodyColor}
-                        zIndex={1}
-                        w={"50px"}
-                      >
-                        <VStack
-                          borderLeft={"1px solid var(--divider3)"}
-                          justify={"center"}
-                        >
-                          <IconButton
-                            h={"72px"}
-                            w={"50px"}
-                            aria-label="Option Button"
-                            icon={<Icon as={RiMore2Fill} fontSize={iconSize} />}
-                            className="btn"
-                            borderRadius={0}
-                          />
-                        </VStack>
-                      </Td>
-                    </Tr>
-                  ))}
-                </Tbody>
-              </Table>
-            )}
-          </TabelContainer>
+                  {fd && fd?.length > 0 && (
+                    <>
+                      <CustomTableContainer>
+                        <CustomTable
+                          formattedHeader={formattedHeader}
+                          formattedData={formattedData}
+                          rowOptions={rowOptions}
+                        />
+                      </CustomTableContainer>
+                    </>
+                  )}
+                </>
+              )}
+            </>
+          )}
         </>
       )}
     </>
--- src/pages/Pengaturan/PengaturanTerPph21.tsx
+++ src/pages/Pengaturan/PengaturanTerPph21.tsx
@@ -1,14 +1,11 @@
-import { Button, HStack, Wrap } from "@chakra-ui/react";
+import { HStack } from "@chakra-ui/react";
 import { useState } from "react";
-import { Link } from "react-router-dom";
+import SearchComponent from "../../components/dependent/input/SearchComponent";
 import TabelTerPph21 from "../../components/dependent/TabelPengaturanTerPph21";
 import TambahTerPph21 from "../../components/independent/TambahTerPph21";
 import CContainer from "../../components/wrapper/CContainer";
-import CWrapper from "../../components/wrapper/CWrapper";
 import { useBodyColor } from "../../const/colors";
-import pengaturanTopNavs from "../../const/pengaturanTopNavs";
 import { responsiveSpacing } from "../../const/sizes";
-import SearchComponent from "../../components/dependent/input/SearchComponent";
 
 export default function PengaturanTerPph21() {
   // Filter Config
@@ -18,66 +15,38 @@ export default function PengaturanTerPph21() {
   const [filterConfig, setFilterConfig] = useState<any>(defaultFilterConfig);
 
   return (
-    <>
-      <CWrapper>
-        <Wrap w={"100%"} spacing={responsiveSpacing} align={"flex-start"}>
-          <CContainer
-            p={4}
-            bg={useBodyColor()}
-            borderRadius={12}
-            w={"230px"}
-            flexShrink={0}
-            gap={2}
-          >
-            {pengaturanTopNavs[2].subNavs?.map((nav, i) => (
-              <Button
-                key={i}
-                justifyContent={"flex-start"}
-                className={i === 1 ? "btn-apa clicky" : "btn clicky"}
-                fontWeight={500}
-                as={Link}
-                to={nav.link}
-                h={"50px"}
-              >
-                {nav.label}
-              </Button>
-            ))}
-          </CContainer>
+    <CContainer
+      p={responsiveSpacing}
+      bg={useBodyColor()}
+      borderRadius={12}
+      flex={"1 1 600px"}
+      overflowX={"auto"}
+      h={"100%"}
+    >
+      <HStack
+        py={responsiveSpacing}
+        justify={"space-between"}
+        w={"100%"}
+        className="tabelConfig scrollX"
+        overflowX={"auto"}
+        flexShrink={0}
+      >
+        <SearchComponent
+          minW={"165px"}
+          name="search"
+          onChangeSetter={(input) => {
+            setFilterConfig((ps: any) => ({
+              ...ps,
+              search: input,
+            }));
+          }}
+          inputValue={filterConfig.search}
+        />
 
-          <CContainer
-            p={responsiveSpacing}
-            bg={useBodyColor()}
-            borderRadius={12}
-            flex={"1 1 600px"}
-            overflowX={"auto"}
-          >
-            <HStack
-              py={responsiveSpacing}
-              justify={"space-between"}
-              w={"100%"}
-              className="tabelConfig scrollX"
-              overflowX={"auto"}
-              flexShrink={0}
-            >
-              <SearchComponent
-                minW={"165px"}
-                name="search"
-                onChangeSetter={(input) => {
-                  setFilterConfig((ps: any) => ({
-                    ...ps,
-                    search: input,
-                  }));
-                }}
-                inputValue={filterConfig.search}
-              />
+        <TambahTerPph21 minW={"fit-content"} />
+      </HStack>
 
-              <TambahTerPph21 minW={"fit-content"} />
-            </HStack>
-
-            <TabelTerPph21 filterConfig={filterConfig} />
-          </CContainer>
-        </Wrap>
-      </CWrapper>
-    </>
+      <TabelTerPph21 filterConfig={filterConfig} />
+    </CContainer>
   );
 }
