d9e4426d9f5e75166f8c5e1c04377b1ef4bd9ecc
sulenq <whyimustwriteit@gmail.com>
2024-07-27 21:46:39 +0700

    styling

--- src/components/independent/AdminMiniProfile.tsx
+++ src/components/independent/AdminMiniProfile.tsx
@@ -3,13 +3,75 @@ import {
   Button,
   HStack,
   Icon,
+  Menu,
+  MenuButton,
+  MenuItem,
+  MenuList,
+  Modal,
+  ModalBody,
+  ModalContent,
+  ModalFooter,
+  ModalHeader,
+  ModalOverlay,
   StackProps,
   Text,
+  useDisclosure,
 } from "@chakra-ui/react";
-import { RiArrowDownSLine } from "@remixicon/react";
-import { useEffect, useState } from "react";
+import { RiArrowDownSLine, RiLogoutBoxLine } from "@remixicon/react";
+import { useEffect, useRef, useState } from "react";
 import { getCookie } from "typescript-cookie";
 import { iconSize } from "../../const/sizes";
+import useBackOnClose from "../../hooks/useBackOnClose";
+import backOnClose from "../../lib/backOnClose";
+import DisclosureHeader from "../dependent/DisclosureHeader";
+
+const LogoutConfirmation = () => {
+  const { isOpen, onOpen, onClose } = useDisclosure();
+  useBackOnClose("logout-confirmation-modal", isOpen, onOpen, onClose);
+
+  return (
+    <>
+      <MenuItem fontWeight={500} onClick={onOpen}>
+        <Text color={"red.400"}>Keluar</Text>
+        <Icon
+          as={RiLogoutBoxLine}
+          fontSize={iconSize}
+          color={"red.400"}
+          transform={"rotate(180deg)"}
+        />
+      </MenuItem>
+
+      <Modal
+        isOpen={isOpen}
+        onClose={backOnClose}
+        isCentered
+        blockScrollOnMount={false}
+      >
+        <ModalOverlay />
+        <ModalContent>
+          <ModalHeader>
+            <DisclosureHeader title={"Keluar"} />
+          </ModalHeader>
+          <ModalBody>
+            <Text>Apakah anda yakin akan keluar akun?</Text>
+          </ModalBody>
+          <ModalFooter gap={2}>
+            <Button
+              w={"100%"}
+              className="btn-solid clicky"
+              onClick={backOnClose}
+            >
+              Tidak
+            </Button>
+            <Button w={"100%"} className="clicky" colorScheme="red">
+              Ya
+            </Button>
+          </ModalFooter>
+        </ModalContent>
+      </Modal>
+    </>
+  );
+};
 
 interface Props extends StackProps {}
 
@@ -22,13 +84,35 @@ export default function AdminMiniProfile({ ...props }: Props) {
     }
   }, []);
 
+  const [menuButtonW, setMenuButtonW] = useState<number | undefined>();
+  const menuButtonRef = useRef<HTMLButtonElement>(null);
+  useEffect(() => {
+    if (menuButtonRef.current) {
+      setMenuButtonW(menuButtonRef.current.offsetWidth);
+    }
+  }, [menuButtonRef]);
+
   return (
-    <HStack as={Button} className="btn-solid clicky" pl={3} {...props}>
-      <Icon as={RiArrowDownSLine} fontSize={iconSize} />
+    <Menu>
+      <MenuButton>
+        <HStack
+          ref={menuButtonRef}
+          as={Button}
+          className="btn-solid"
+          pl={3}
+          {...props}
+        >
+          <Icon as={RiArrowDownSLine} fontSize={iconSize} />
 
-      <Text>{data?.name || "Admin"}</Text>
+          <Text>{data?.name || "Personalia"}</Text>
 
-      <Avatar name={data?.name} size={"xs"} />
-    </HStack>
+          <Avatar name={data?.name} size={"xs"} />
+        </HStack>
+      </MenuButton>
+
+      <MenuList minW={menuButtonW}>
+        <LogoutConfirmation />
+      </MenuList>
+    </Menu>
   );
 }
